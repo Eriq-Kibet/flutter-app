d6fe4d6462b209ddcf9187ad5620c12d2dad7e85
Eric <ericthedev@gmail.com>
2022-03-30 16:53:07 -0400

    home Styling

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -12,20 +12,33 @@ class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      child: Container(
-        child: Row(children: [
-          Column(
-            children: [Text("Coutnry"), Text("City")],
+        body: Column(
+      children: [
+        Container(
+          child: Container(
+            margin: EdgeInsets.only(top: 45, bottom: 15),
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [Text("Coutnry"), Text("City")],
+                  ),
+                  Container(
+                    width: 45,
+                    height: 45,
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.blue),
+                  )
+                ]),
           ),
-          Container(
-            width: 45,
-            height: 45,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15), color: Colors.blue),
-          )
-        ]),
-      ),
+        )
+      ],
     ));
   }
 }
